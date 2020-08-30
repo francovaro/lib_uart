58886b833c11a8a0bbfe38a842c97800cbb2111b
Francesco <francesco-varani@wavecontrol.com>
2020-08-30 17:32:15 +0200

    which usart as parameter

--- src/lib_uart.c
+++ src/lib_uart.c
@@ -5,64 +5,114 @@
  *      Author: franc
  */
 #include "lib_uart.h"
+#include "stddef.h"
 
 static void USART2_NVIC_Config(void);
+
+static FunctionalState _USART_config [t_UART_lib_] = {DISABLE};
 
 /**
  *
  * @param uart_to_config
  * @param irqEnabled
  */
-void UART_lib_config(t_UART_lib_ uart_to_config, uint8_t irqEnabled)
+ErrorStatus UART_lib_config(t_UART_lib_ uart_to_config, uint8_t irqEnabled)
 {
-	GPIO_InitTypeDef GPIO_InitStruct;
-	USART_InitTypeDef USART_InitStructure;
+	ErrorStatus			config = SUCCESS;
+	GPIO_InitTypeDef 	GPIO_InitStruct;
+	USART_InitTypeDef 	USART_InitStructure;
+	USART_TypeDef*		_usart_to_use = NULL;
 
 	usart_serialReceived = 0;
 	usart_nrOfByte = 0;
 
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-
 	GPIO_StructInit(&GPIO_InitStruct);
 	USART_StructInit(&USART_InitStructure);
 
-	// rx PA10
-	// tx PA9
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
-
-	// Initialize USART2_Tx
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_Init(GPIOA , &GPIO_InitStruct);
-
-	 /*!< This member configures the USART communication baud rate.
-	     The baud rate is computed using the following formula:
-
-	     - IntegerDivider = ((PCLKx) / (8 * (OVR8+1) * (USART_InitStruct->USART_BaudRate)))
-	     - FractionalDivider = ((IntegerDivider - ((u32) IntegerDivider)) * 8 * (OVR8+1)) + 0.5
-	 Where OVR8 is the "oversampling by 8 mode" configuration bit in the CR1 register. */
-
-	USART_InitStructure.USART_BaudRate = 115200;
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-	USART_InitStructure.USART_Parity = USART_Parity_No;
-	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_Init(USART2 ,&USART_InitStructure);
-
-	if(irqEnabled == ENABLE)
+	if ((_USART_config[uart_to_config] == ENABLE)
+			|| uart_to_config >= e_UART_None)
 	{
-		//USART2
-		USART_ITConfig(USART2, USART_IT_RXNE, ENABLE); /* ENABLE INTERRUPT ON rx */
-		USART2_NVIC_Config();
+		config = ERROR;
+	}
+	else
+	{
+		switch(uart_to_config)
+		{
+			case e_UART_1:
+			{
+
+			}
+			break;
+			case e_UART_2:
+			{
+				RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+				RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+				// rx PA10
+				// tx PA9
+				GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+				GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
+
+				// Initialize USART2_Tx
+				GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+
+				_usart_to_use = (USART_TypeDef *)USART2;
+			}
+			break;
+			case e_UART_3:
+			{
+
+			}
+			break;
+			case e_UART_4:
+			{
+
+			}
+			break;
+			case e_UART_5:
+			{
+
+			}
+			break;
+			default:
+			case e_UART_None:
+			{
+				/* theoretically can't fall here */
+			}
+			break;
+		}
+
+		GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+		GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+		GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+		GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+		GPIO_Init(GPIOA , &GPIO_InitStruct);
+
+		 /*!< This member configures the USART communication baud rate.
+		     The baud rate is computed using the following formula:
+
+		     - IntegerDivider = ((PCLKx) / (8 * (OVR8+1) * (USART_InitStruct->USART_BaudRate)))
+		     - FractionalDivider = ((IntegerDivider - ((u32) IntegerDivider)) * 8 * (OVR8+1)) + 0.5
+		 Where OVR8 is the "oversampling by 8 mode" configuration bit in the CR1 register. */
+
+		USART_InitStructure.USART_BaudRate = 115200;
+		USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+		USART_InitStructure.USART_StopBits = USART_StopBits_1;
+		USART_InitStructure.USART_Parity = USART_Parity_No;
+		USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+		USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+		USART_Init(_usart_to_use ,&USART_InitStructure);
+
+		if(irqEnabled == ENABLE)
+		{
+			USART_ITConfig(_usart_to_use, USART_IT_RXNE, ENABLE); /* ENABLE INTERRUPT ON rx */
+			USART2_NVIC_Config();
+		}
+
+		USART_Cmd(_usart_to_use , ENABLE);
 	}
 
-	USART_Cmd(USART2 , ENABLE);
+	return config;
 }
 
 /**
